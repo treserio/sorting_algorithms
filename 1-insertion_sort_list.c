015112f27423efa7f2f201c089421ced12782463
Tres Serio <treserio@hotmail.com>
2022-03-08 15:49:21 -0600

    1 init

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *run, *swap;
+	/* check for 1 value in list */
+	if (!list || !*list || !(*list)->next)
+		return;
+	/* move forward and check back for values */
+	for (run = (*list)->next; run; run = run -> next)
+	{
+		for (swap = run; swap->prev && swap->prev->n > swap->n;)
+		{
+			if (swap->prev->prev)
+			{
+				swap->prev->prev->next = swap;
+				if (swap->next)
+				{
+					swap->prev->next = swap->next;
+					swap->next->prev = swap->prev;
+				}
+				else
+					swap->prev->next = NULL;
+				swap->next = swap->prev;
+				swap->prev = swap->next->prev;
+				swap->next->prev = swap;
+			}
+			else
+			{
+				if (swap->next)
+				{
+					swap->prev->next = swap->next;
+					swap->next->prev = swap->prev;
+				}
+				else
+					swap->prev->next = NULL;
+				swap->prev->prev = swap;
+				swap->next = swap->prev;
+				swap->prev = NULL;
+				*list = swap;
+			}
+			print_list(*list);
+		}
+	}
+}
+
